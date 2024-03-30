8c6d34f059563abd480a19d2a55cdd2ab79fc14d
galilei <galilei.mail@gmail.com>
2024-03-29 20:11:29 -0700

    Skeleton of the web app

---
+++ web/src/routes/+page.svelte
@@ -0,0 +1,17 @@
+
+ <div class="h-screen flex"> <div class="w-1/12 bg-gray-200">
+      <button class="block m-4 p-2 bg-blue-500 text-white rounded">New Chat</button>
+      <ul class="p-4"> <li class="mb-2"><a href="/" class="hover:bg-gray-300 block p-2 rounded">Thread 1</a></li>
+        <li class="mb-2"><a href="/" class="hover:bg-gray-300 block p-2 rounded">Thread 2</a></li>
+        </ul>
+    </div>
+
+    <div class="w-11/12 overflow-y-auto"> <div class="p-4">
+
+    </div>
+      
+      <div class="fixed bottom-0 w-full p-4 bg-gray-100">
+        <input type="text" class="w-full p-2 rounded border" placeholder="Type your message...">
+      </div>
+    </div>
+  </div>
