77a9fad7684e6bd4e5d0760cf8c73e6526a171ea
galilei <galilei.mail@gmail.com>
2024-04-09 11:27:17 -0700

    TOS and Privacy policy

---
+++ web/src/lib/components/LegalPage.svelte
@@ -0,0 +1,52 @@
+<div class="mx-auto w-8/12 min-w-[1024px] py-8">
+    <style>
+        a {
+            text-decoration: underline;
+        }
+
+        h1 {
+            font-size: 2rem;
+            color: #111;
+            margin-top: 1.5rem;
+            margin-bottom: 0.5rem;
+        }
+
+        h2 {
+            font-size: 1.75rem;
+            color: #333;
+            margin-top: 1.5rem;
+            margin-bottom: 0.5rem;
+        }
+
+        h3 {
+            font-size: 1.5rem;
+            color: #555;
+            margin-top: 1.5rem;
+            margin-bottom: 0.5rem;
+        }
+
+        h4 {
+            font-size: 1.25rem;
+            color: #777;
+            margin-top: 1.5rem;
+            margin-bottom: 0.5rem;
+        }
+
+        p, li {
+            font-size: 1rem;
+            color: #4d4d4d;
+            line-height: 1.6;
+        }
+
+        ul {
+            padding-inline-start: 2rem;
+        }
+
+        li {
+            list-style-type: disc;
+            margin-bottom: 0.5rem;
+        }
+    </style>
+
+    <slot></slot>
+</div>
