fc65717299dee7c100e9539ef9302b0e2b5f92a7
galilei <galilei.mail@gmail.com>
2024-04-13 14:13:41 -0700

    Start AirTable integration

--- bot/http_routes/conversation.py
+++ bot/http_routes/conversation.py
@@ -1,9 +1,20 @@
 from google.cloud import firestore
 from core import OpenAiClient
+from pyairtable import Api
+
+open_ai = OpenAiClient()
+api = Api('pat9apNtUsG6DD0TU.b45f4ff13db65d5e8861469a8804a5eb043a4ec7bb0478fa40c8307a7c41bfc3')
+table = api.table("appv61Zx3hTzrhbPk", 'tblxVDuRS2nPskWXd')
 
 
-open_ai =  OpenAiClient()
+def make_message_from_row(row):
+    fields = row.get('fields') or []
+    content = f"# Task {fields['ID']:\n\n}"
+    lines = '\n'.join([f"{k}:{v}" for k, v in fields.items()])
+    return '\n'.join('')
 
+def get_context_messages():
+    table.all()
 
 async def next_message(
         user_id: str,
@@ -13,7 +24,7 @@ async def next_message(
     query = thread_doc_ref.collection('messages').order_by('created_at')
     history = [doc.to_dict() async for doc in query.stream()]
     assert len(history) > 2, "History must be more than 2 messages "
-    history, last_message, ai_message = history[:-3], history[-2], history[-1]
+    history, last_message, ai_message = history[-10:-3], history[-2], history[-1]
     history = [{'role': doc['role'], 'content': doc['content']} for doc in history]
     async for text in open_ai.continue_chat(
         user_id=user_id,
